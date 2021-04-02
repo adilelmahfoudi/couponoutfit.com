b5c2682b32acbd33dcb6482ab8b7ff93c7207997
mahfoudiadil1980 <81696549+mahfoudiadil1980@users.noreply.github.com>
2021-04-03 00:33:00 +0200

    Initial commit

---
+++ aboutus.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+ <head>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>About us - Mydiscountstuff.com</title>
+<meta name="description" content="About us -  CouponOutfit.com." />
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">   
+<meta name="msvalidate.01" content="8E2C7BD7158AE8D0AA1ADA1576B1BD91" />
+</head>
+
+  <body>
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+         <a href="http://www.couponoutfit.com/"> 
+		  		 <img src="couponoutfit.jpg"  alt="..."></a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">		  
+			<li ><a href="http://www.couponoutfit.com/">Home</a></li>
+			<li><a href="http://www.couponoutfit.com/allstore.php">By Store</a></li>   
+         </ul>
+		 
+		  <div class="col-sm-3 col-md-3 pull-right">
+           <form class="navbar-form" role="search" action="search.php">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search" name="searchterm">
+                    <div class="input-group-btn">
+                        <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                    </div>
+                </div>
+            </form>
+        </div> 
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item ">
+      <img src="home_1.jpg" width="100%" alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    <div class="item active">
+      <img src="home_2.jpg" width="100%" alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+	
+    ...
+  </div>
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+  
+	<section id="title-one">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <h1>About us</h1>
+			
+
+           </div>
+        </div>
+      </div>
+    </section>
+<section id="middle">
+<div class="container">
+<div class="row">	
+ <div class="col-md-8">
+ 
+    <p> The mere aim of Couponoutfit.com is to help online shoppers save time and money every day!</p>
+			<p>				
+Couponoutfit.com is one of the best places to find online coupons, deals, and special offers. At Couponoutfit.com, you can find everything that offers you significant savings and help you save money through our comprehensive listing of coupons, offers, deals and discounts from top online brands and websites.
+
+			</p>
+			
+			<p>				
+At Couponoutfit.com, we update coupons, deals, and special offers everyday. Out of millions of products offered by thousands of stores, we find the best deals every day and on weekends we get you more better deals. Our sole mission is to create a generation of smart shoppers by empowering customers to make the best possible choices.
+
+			</p>
+			<p>In case you want to contact us, visit our Contact Us page or drop us a mail at contact@couponoutfit.com.</p>
+ </div> 
+		
+
+
+</div>
+	</div>	
+	</section> 
+<p>
+
+   	
+
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-xs-12">
+            <ul>
+           <li><a href="http://www.couponoutfit.com/">Home</a></li>
+              <li><a href="http://www.couponoutfit.com/aboutus.php">About</a></li>
+              <li><a href="http://www.couponoutfit.com/contactus.php">Contact</a></li>
+              <li><a href="http://www.couponoutfit.com/term.php">Term & Privacy Policy</a></li>
+            </ul>
+          </div>
+          <div class="col-md-6 col-xs-12">
+            <p>Copyright &copy; 2015, All Rights Reserved</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+  </body>
+</html>
---
+++ contactus.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+ <head>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Contact us - CouponOutfit.com</title>
+<meta name="description" content="Contact us - CouponOutfit.com." />
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">   
+<meta name="msvalidate.01" content="8E2C7BD7158AE8D0AA1ADA1576B1BD91" />
+</head>
+
+  <body>
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+               <a href="http://www.couponoutfit.com/"> 
+		  		 <img src="couponoutfit.jpg"  alt="..."></a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+           <ul class="nav navbar-nav">		  
+			<li ><a href="http://www.couponoutfit.com/">Home</a></li>
+			<li><a href="http://www.couponoutfit.com/allstore.php">By Store</a></li>      
+         </ul>
+		  <div class="col-sm-3 col-md-3 pull-right">
+            <form class="navbar-form" role="search" action="search.php">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search" name="searchterm">
+                    <div class="input-group-btn">
+                        <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                    </div>
+                </div>
+            </form>
+        </div> 
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+	
+	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item ">
+      <img src="home_1.jpg" width="100%" alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    <div class="item active">
+      <img src="home_2.jpg" width="100%" alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+	
+    ...
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+   
+	<section id="title-one">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <h1>Contact us</h1>
+			
+
+           </div>
+        </div>
+      </div>
+    </section>
+<section id="middle">
+<div class="container">
+<div class="row">	
+ <div class="col-md-8">
+ 
+     
+			<form>
+              <div class="form-group">
+                <label>Name</label>
+                <input type="text" class="form-control" placeholder="Enter Name">
+              </div>
+              <div class="form-group">
+                <label>Email address</label>
+                <input type="email" class="form-control" placeholder="Enter Email">
+              </div>
+              <div class="form-group">
+                <label>Message</label>
+                <textarea class="form-control" placeholder="Enter Message"></textarea>
+              </div>
+
+              <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+ </div> 
+		
+
+
+
+
+</div>
+	</div>	
+	</section> 
+<p>
+
+    
+	
+	
+
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-xs-12">
+            <ul>
+             <li><a href="http://www.couponoutfit.com/">Home</a></li>
+              <li><a href="http://www.couponoutfit.com/aboutus.php">About</a></li>
+              <li><a href="http://www.couponoutfit.com/contactus.php">Contact</a></li>
+              <li><a href="http://www.couponoutfit.com/term.php">Term & Privacy Policy</a></li>
+            </ul>
+          </div>
+          <div class="col-md-6 col-xs-12">
+            <p>Copyright &copy; 2015, All Rights Reserved</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+  </body>
+</html>
